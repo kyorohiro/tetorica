600f3aacca8daacf963398dfc1301cee35ee0c0b
kyorohiro <kyorohiro@gmail.com>
2016-02-01 00:40:52 +0900

    rename ...

--- lib/net.dart
+++ lib/net.dart
@@ -3,5 +3,4 @@ library hetimanet;
 export 'net/net/hetisocket.dart';
 export 'net/http/hetihttp.dart';
 export 'net/util/hetiutil.dart';
-export 'net/upnp/hetiupnp.dart';
 export 'net/net/hetisocket_simu.dart';
