70b4d69b602af692e22ac21066fa1bc5508e8041
kyorohiro <kyorohiro@gmail.com>
2016-01-31 21:19:36 +0900

    ..

---
+++ test_plus/lib/alltest.dart
@@ -0,0 +1,10 @@
+
+//import 'package:tetorica/core.dart' as hetima;
+//import 'package:tetorica/hetimanet.dart' as hetima;
+import 'package:unittest/unittest.dart' as unit;
+import 'package:tetorica/core.dart';
+import 'dart:async';
+
+main() {
+
+}
