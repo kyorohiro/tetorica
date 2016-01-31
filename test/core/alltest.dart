5c173d7633ca3fd2333279b92623a967b8b70909
kyorohiro <kyorohiro@gmail.com>
2016-02-01 02:03:29 +0900

    add testcase

--- test/core/alltest.dart
+++ test/core/alltest.dart
@@ -4,10 +4,16 @@ import './easyparser_test.dart' as t_easyparser;
 import './hetimacore_dartio_test.dart' as t_coredartio;
 import './percentencode_test.dart' as t_persent;
 
+import './bencode_test.dart' as bencode_test;
+import './bencode_test2.dart' as bencode_test2;
 void main() {
   t_arraybuilder.main();
   t_arrraybuilder_z99.main();
   t_easyparser.main();
   t_coredartio.main();
   t_persent.main();
+
+  //
+  bencode_test.main();
+  bencode_test2.main();
 }
