c0db382101c49049c90e7083648534604f27d8ac
kyorohiro <kyorohiro@gmail.com>
2016-03-06 01:03:43 +0900

    add put test

--- test_plus/bin/main.dart
+++ test_plus/bin/main.dart
@@ -33,4 +33,16 @@ testA(TetSocketBuilder builder) async {
     test.expect("nano", (ret["headers"] as Map)["Nono"]);
     //test.expect("hello!!", (ret["json"] as Map)["message"]);
   });
+
+  test.test("put", () async {
+    http.HttpClient client = new http.HttpClient(builder);
+    await client.connect("httpbin.org", 80);
+    http.HttpClientResponse postResult = await client.put("/put", conv.UTF8.encode(conv.JSON.encode({"message": "hello!!"})), header: {"nono": "nano", "Content-Type": "application/json"});
+    print("## ${postResult.message.contentLength}");
+    print("## ${await postResult.body.getString()}");
+    Map<String, Object> ret = conv.JSON.decode(await postResult.body.getString());
+    test.expect("application/json", (ret["headers"] as Map)["Content-Type"]);
+    test.expect("nano", (ret["headers"] as Map)["Nono"]);
+    test.expect("hello!!", (ret["json"] as Map)["message"]);
+  });
 }
