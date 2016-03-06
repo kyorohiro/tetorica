bb7113e589c5978428fabd41af414348aa0dfe3f
kyorohiro <kyorohiro@gmail.com>
2016-03-06 15:40:04 +0900

    ..

--- lib/http/httpclient_helper.dart
+++ lib/http/httpclient_helper.dart
@@ -14,6 +14,26 @@ class HttpClientHelper {
       useSecure:useSecure);
   }
 
+  Future<HttpClientResponse> delete(String address, int port, String pathAndOption,
+    {List<int> redirectStatusCode: const [301, 302, 303, 304, 305, 307, 308],
+       Map<String, String> header, int redirect: 5,
+       bool reuseQuery: true, bool useSecure:false}) async {
+      return await base(address, port, "DELETE", pathAndOption, null,
+      redirectStatusCode: redirectStatusCode,
+      header: header, redirect: redirect,
+      reuseQuery: reuseQuery,
+      useSecure:useSecure);
+  }
+  Future<HttpClientResponse> head(String address, int port, String pathAndOption,
+    {List<int> redirectStatusCode: const [301, 302, 303, 304, 305, 307, 308],
+       Map<String, String> header, int redirect: 5,
+       bool reuseQuery: true, bool useSecure:false}) async {
+      return await base(address, port, "HEAD", pathAndOption, null,
+      redirectStatusCode: redirectStatusCode,
+      header: header, redirect: redirect,
+      reuseQuery: reuseQuery,
+      useSecure:useSecure,isLoadBody:false);
+  }
   Future<HttpClientResponse> post(String address, int port, String pathAndOption, List<int> data,
     {List<int> redirectStatusCode: const [301, 302, 303, 304, 305, 307, 308],
        Map<String, String> header, int redirect: 5,
@@ -25,16 +45,40 @@ class HttpClientHelper {
       useSecure:useSecure);
   }
 
+  Future<HttpClientResponse> put(String address, int port, String pathAndOption, List<int> data,
+    {List<int> redirectStatusCode: const [301, 302, 303, 304, 305, 307, 308],
+       Map<String, String> header, int redirect: 5,
+       bool reuseQuery: true, bool useSecure:false}) async {
+      return await base(address, port, "PUT", pathAndOption, data,
+      redirectStatusCode: redirectStatusCode,
+      header: header, redirect: redirect,
+      reuseQuery: reuseQuery,
+      useSecure:useSecure);
+  }
+
+  Future<HttpClientResponse> patch(String address, int port, String pathAndOption, List<int> data,
+    {List<int> redirectStatusCode: const [301, 302, 303, 304, 305, 307, 308],
+       Map<String, String> header, int redirect: 5,
+       bool reuseQuery: true, bool useSecure:false}) async {
+      return await base(address, port, "PATCH", pathAndOption, data,
+      redirectStatusCode: redirectStatusCode,
+      header: header, redirect: redirect,
+      reuseQuery: reuseQuery,
+      useSecure:useSecure);
+  }
+
   Future<HttpClientResponse> base(String address, int port, String action, String pathAndOption,
      List<int> data,
     {
       List<int> redirectStatusCode: const [301, 302, 303, 304, 305, 307, 308],
       Map<String, String> header, int redirect: 5, bool reuseQuery: true,
-      bool useSecure:false}) async {
+      bool useSecure:false, isLoadBody:true}) async {
     print("${pathAndOption}");
     HttpClient client = new HttpClient(socketBuilder);
     await client.connect(address, port, useSecure:useSecure);
-    HttpClientResponse res = await client.base(action,pathAndOption, data, header:header);
+    HttpClientResponse res = await client.base(action,pathAndOption, data,
+      header:header,
+      isLoadBody:isLoadBody);
     client.close();
     //
     if (redirectStatusCode.contains(res.message.line.statusCode)) {
