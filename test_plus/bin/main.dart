b3c7f47bc90dccd71c25f2df2b7870f70282e01d
kyorohiro <kyorohiro@gmail.com>
2016-03-08 23:00:06 +0900

    ..

--- test_plus/bin/main.dart
+++ test_plus/bin/main.dart
@@ -1,8 +1,9 @@
 import 'package:tetorica/net_dartio.dart';
+import 'package:test_plus/http.dart' as http;
 import 'package:test_plus/networkinterface.dart' as networkinterface;
 
 main() async {
   TetSocketBuilderDartIO builder = new TetSocketBuilderDartIO();
-//  http.doTest(builder);
+  http.doTest(builder);
   networkinterface.doTest(builder);
 }
