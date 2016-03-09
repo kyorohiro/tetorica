d11b19cc96c38d74f0f106219508617c2b7870a3
kyorohiro <kyorohiro@gmail.com>
2016-03-09 17:09:04 +0900

    ..

--- test_plus/lib/stun.dart
+++ test_plus/lib/stun.dart
@@ -5,7 +5,9 @@ import 'package:tetorica/net.dart' as net;
 import 'package:tetorica/stun.dart' as stun;
 
 doTest(net.TetSocketBuilder builder) async {
-//  stun.StunClient client = new stun.StunClient(builder, "183.181.26.146", 19302);
-  stun.StunClient client = new stun.StunClient(builder, "stun.l.google.com", 19302);
+  //  -p sets the primary port and defaults to 3478
+  //-o sets the secondary port and defaults to 3479
+  stun.StunClient client = new stun.StunClient(builder, "183.181.26.146", 3478);
+//  stun.StunClient client = new stun.StunClient(builder, "stun.l.google.com", 19302);
   client.test001();
 }
