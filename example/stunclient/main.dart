af2efeb95ee6dc01b4626d5bdd173e3804519b0a
kyorohiro <kyorohiro@gmail.com>
2016-03-11 22:43:20 +0900

    ..

--- example/stunclient/main.dart
+++ example/stunclient/main.dart
@@ -4,31 +4,40 @@ import 'package:tetorica/stun.dart' as stun;
 
 main(List<String> args) async {
   net.TetSocketBuilder builder = new dartio.TetSocketBuilderDartIO();
-  if (args.length != 2) {
-    print("dart xxx.dart [primary ip] [primary port]");
+  if (args.length < 2) {
+    print("dart xxx.dart <primary ip> <primary port> [<client ip>] ");
     return;
   }
   String primaryIP = args[0];
   int primaryPort = int.parse(args[1]);
 
+  String clientIp = null;
+  if(args.length >= 3) {
+    clientIp = args[2];
+  }
+
+  if(clientIp == null) {
+    print("[${clientIp}]");
+    await checkIP(builder, clientIp, primaryIP, primaryPort);
+    return;
+  }
   for (net.TetNetworkInterface i in await builder.getNetworkInterfaces()) {
     print("[${i}]");
-    net.IPAddr addr = new net.IPAddr.fromString(i.address);
-    if (addr.isLocalHost() == true || addr.isLinkLocal() == true || addr.isV6() == true) {
-      continue;
-    }
-    try {
-      print("   ${addr.toString()}");
-      stun.StunClient client = new stun.StunClient(builder, addr.toString(), 18081, primaryIP, primaryPort);
-      print("   ${await client.testStunType()}");
-    } catch (e) {
-      ;
-    }
-    print("\n");
+    await checkIP(builder, i.address, primaryIP, primaryPort);
   }
-//  stun.StunClient client = new stun.StunClient(builder, "127.0.0.1", 18081, primaryIP, primaryPort);
-//  await client.testStunType();
+}
 
-//  stun.StunClient client = new stun.StunClient(builder, primaryIP, primaryPort);
-//  client.testStunType(ipList);
+checkIP(net.TetSocketBuilder builder, String address, String primaryIP, int primaryPort) async {
+  net.IPAddr addr = new net.IPAddr.fromString(address);
+  if (addr.isLocalHost() == true || addr.isLinkLocal() == true || addr.isV6() == true) {
+    return;
+  }
+  try {
+    print("   ${addr.toString()}");
+    stun.StunClient client = new stun.StunClient(builder, addr.toString(), 18081, primaryIP, primaryPort);
+    print("   ${await client.testStunType()}");
+  } catch (e) {
+    ;
+  }
+  print("\n");
 }
