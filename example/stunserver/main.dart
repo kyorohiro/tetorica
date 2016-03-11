fade27c77e91f8b29f6d30172f47d3a392efa658
kyorohiro <kyorohiro@gmail.com>
2016-03-11 14:58:19 +0900

    ..

--- example/stunserver/main.dart
+++ example/stunserver/main.dart
@@ -6,11 +6,18 @@ main(List<String> args) async {
   net.TetSocketBuilder builder = new dartio.TetSocketBuilderDartIO();
   if(args.length != 4) {
     print("dart xxx.dart [primary ip] [primary port] [secondary ip] [secondary port]");
-    
     return;
-  } else {
-
   }
+  String primaryIP = args[0];
+  int primaryPort = int.parse(args[1]);
+  String secondaryIP = args[2];
+  int secondaryPort = int.parse(args[3]);
 
+  stun.StunServer server = new stun.StunServer(builder,
+    primaryIP,
+    primaryPort,
+    secondaryIP,
+    secondaryPort);
+  server.go();
 
 }
