f2aa757ae3cf4d7dac87b10c17705678067221a5
kyorohiro <kyorohiro@gmail.com>
2016-03-18 01:23:59 +0900

    mod base64

--- lib/cipher/base64.dart
+++ lib/cipher/base64.dart
@@ -9,8 +9,8 @@ class Base64 {
   static final List<int> unbase64Bytes = const [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, 0, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1, -1];
   static final int equalByte = ASCII.encode("=")[0];
 
-  static List<int> encode(List<int> input, int sourceIndex, Result result) {
-    int len = input.length - sourceIndex, end = len - (len % 3);
+  static List<int> encode(List<int> input, int sourceIndex, int sourceLength, Result result) {
+    int len = sourceLength - sourceIndex, end = len - (len % 3);
     int resultIndex = result.index;
     List<int> buffer = result.buffer;
 
@@ -31,12 +31,17 @@ class Base64 {
       case 2:
         buffer[ri++] = base64Bytes[0x3f & (input[si] >> 2)];
         buffer[ri++] = base64Bytes[0x3f & ((input[si] & 0x03) << 4)];
-        buffer[ri++] = base64Bytes[0x3f & (((input[si + 1] << 2) & 0x3C) | (input[si + 2] >> 6) & 0x03)];
+        buffer[ri++] = base64Bytes[0x3f & (((input[si + 1] << 2) & 0x3C) & 0x03)];
         break;
       default:
     }
-    for (int j = 0, len = ri % 4; j < len; j++) {
-      buffer[ri + j] = equalByte;
+
+    int l = ((ri-resultIndex) % 4);
+    if (l != 0) {
+      l = 4-l;
+      for (int j = 0; j < l; j++) {
+        buffer[ri++] = equalByte;
+      }
     }
     result.length = ri - result.index;
     return buffer;
@@ -48,6 +53,7 @@ class Base64 {
     int si = sourceIndex;
     int ri = resultIndex;
     int v1, v2, v3, v4 = 0;
+
     for (int end = sourceIndex + sourceLength; si < end; si += 4) {
       v1 = unbase64Bytes[input[si]];
       v2 = unbase64Bytes[input[si + 1]];
@@ -57,10 +63,15 @@ class Base64 {
       buffer[ri++] = ((v2 << 4) & 0xf0) | ((v3 >> 2) & 0xf);
       buffer[ri++] = ((v3 << 6) & 0xf0) | (v4 & 0x3f);
     }
+    if (equalByte == input[sourceIndex + sourceLength - 3]) {
+      ri -= 2;
+    } else if (equalByte == input[sourceIndex + sourceLength - 2]) {
+      ri -= 2;
+    }
+
     result.length = ri - result.index;
     return buffer;
   }
-
 }
 
 class Result {
@@ -68,7 +79,6 @@ class Result {
   int index;
   int length;
   Result(this.index, this.length) {
-    this.index = 0;
     this.buffer = new Uint8List(length);
   }
 
--- test/cipher/base64_test.dart
+++ test/cipher/base64_test.dart
@@ -1,15 +1,69 @@
 import 'package:tetorica/cipher/base64.dart';
 import 'dart:async';
 import 'dart:convert';
+import 'package:test/test.dart' as test;
 
 main() {
-  Result r = new Result(0, 100);
-  List<int> v = Base64.encode(ASCII.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"), 0, r);
-  print(ASCII.decode(v));
-  print("${r.index} ${r.length}");
+  test.group("base64", () {
+    test.test("encode A", () {
+      Result r = new Result(0, 100);
+      List<int> inputValue = ASCII.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+      List<int> v = Base64.encode(inputValue, 0, inputValue.length, r);
+      print(ASCII.decode(v));
+      print("${r.index} ${r.length}");
+      test.expect(ASCII.decode(v.sublist(r.index, r.index + r.length)), "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLw==");
+    });
+    test.test("decode A", () {
+      Result r = new Result(0, 100);
+      List<int> inputValue = ASCII.encode("QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLw==");
+      List<int> w = Base64.decode(inputValue, 0, inputValue.length, r);
+      print("${r.index} ${r.length}");
+      print(ASCII.decode(w.sublist(0, r.length), allowInvalid: true));
+      test.expect(ASCII.decode(w.sublist(r.index, r.index + r.length)), "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+    });
 
-  List<int> inputValue = ASCII.encode("QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLw==");
-  List<int> w = Base64.decode(inputValue, 0, inputValue.length, r);
-  print("${r.index} ${r.length}");
-  print(ASCII.decode(w.sublist(0, r.length), allowInvalid: true));
+    test.test("encode B", () {
+      Result r = new Result(0, 500);
+      List<int> inputValue = [];
+      for (int i = 0; i <= 0xff; i++) {
+        inputValue.add(i);
+      }
+      List<int> v = Base64.encode(inputValue, 0, inputValue.length, r);
+      v = v.sublist(r.index, r.index + r.length);
+      List<int> w = Base64.decode(v, 0, v.length, r);
+      w = w.sublist(r.index, r.index + r.length);
+      test.expect(inputValue, w);
+    });
+
+    test.test("encode E", () {
+      for (int j = 0; j < 0xff; j++) {
+        Result r = new Result(0, 500);
+        List<int> inputValue = [];
+        for (int i = j; i <= 0xff; i++) {
+          inputValue.add(i);
+        }
+        List<int> v = Base64.encode(inputValue, 0, inputValue.length, r);
+        v = v.sublist(r.index, r.index + r.length);
+        List<int> w = Base64.decode(v, 0, v.length, r);
+        w = w.sublist(r.index, r.index + r.length);
+      }
+    });
+
+    test.test("encode F", () {
+      int s = 1;
+      for (int s = 0; s < 20; s++) {
+        for (int j = 0; j < 0xdd; j++) {
+          Result r = new Result(s, 500);
+          List<int> inputValue = [];
+          for (int i = j; i <= 0xff; i++) {
+            inputValue.add(i);
+          }
+          List<int> v = Base64.encode(inputValue, s, inputValue.length, r);
+          v = v.sublist(0, r.index + r.length);
+          List<int> w = Base64.decode(v, s, v.length - s, r);
+          w = w.sublist(r.index, r.index + r.length);
+        }
+      }
+    });
+  });
 }
