b8a49ff51684e120abb95b675763bd9fa58df9cd
kyorohiro <kyorohiro@gmail.com>
2016-03-21 05:13:49 +0900

    ..

--- test/cipher/bigint_test.dart
+++ test/cipher/bigint_test.dart
@@ -7,9 +7,17 @@ import 'dart:typed_data';
 main() {
   test.group("bigint", () {
     test.test("[+]", () {
-      BigInt v1 = new BigInt.fromInt(0xf, 4);
-      BigInt v2 = new BigInt.fromInt(0xf, 4);
-      test.expect("${v1+v2}", "0x000000000000001e");
+      {
+        BigInt v1 = new BigInt.fromInt(0xf, 4);
+        BigInt v2 = new BigInt.fromInt(0xf, 4);
+        test.expect("${v1+v2}", "0x000000000000001e");
+      }
+
+      {
+        BigInt v1 = new BigInt.fromInt(0x1, 4);
+        BigInt v2 = new BigInt.fromInt(0xffff, 4);
+        test.expect("${v1+v2}", "0x0000000000010000");
+      }
     });
     test.test("[-]", () {
       {
