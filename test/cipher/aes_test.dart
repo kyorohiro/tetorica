1e992e2b13eee0c0984595d493445a06f94f802d
kyorohiro <kyorohiro@gmail.com>
2016-03-21 00:24:42 +0900

    decrypt 256bit cbc mode

--- test/cipher/aes_test.dart
+++ test/cipher/aes_test.dart
@@ -159,5 +159,30 @@ main() {
           "b2eb05e2c39be9fcda6c19078c6a9d1b");
       }
     });
+
+    test.test("decrypt CBC 4 256bit", () {
+      // 16bit
+      {
+        List<int> key = Hex.decodeWithNew(
+          "0x603deb1015ca71be2b73aef0857d7781"+
+          "1f352c073b6108d72d9810a30914dff4");
+        List<int> iv = Hex.decodeWithNew(
+          "0x000102030405060708090a0b0c0d0e0f");
+        List<int> cipherText = Hex.decodeWithNew(
+          "0xf58c4c04d6e5f1ba779eabfb5f7bfbd6" +
+            "9cfc4e967edb808d679f777bc6702c7d" +
+            "39f23369a9d9bacfa530e26304231461" +
+            "b2eb05e2c39be9fcda6c19078c6a9d1b");
+
+        List<int> output = new Uint8List(16 * 4);
+
+        AES.decryptWithCBC(cipherText, iv, key, output);
+        test.expect(Hex.encodeWithNew(output),
+        "0x6bc1bee22e409f96e93d7e117393172a" +
+        "ae2d8a571e03ac9c9eb76fac45af8e51" +
+        "30c81c46a35ce411e5fbc1191a0a52ef" +
+        "f69f2445df4f9b17ad2b417be66c3710");
+      }
+    });
   });
 }
