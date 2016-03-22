814e66cad65aa4d5e40e89ac2bf580d395c84274
kyorohiro <kyorohiro@gmail.com>
2016-03-23 07:50:46 +0900

    ..

--- lib/cipher/rsa.dart
+++ lib/cipher/rsa.dart
@@ -7,15 +7,6 @@ import 'bigint.dart';
 // (m**e)**d %n = m
 //   d private, e n public
 class RSA {
-  static BigInt exp(BigInt m, BigInt e) {
-    BigInt c = new BigInt.fromBigInt(m);
-    BigInt counter = new BigInt.fromInt(1, m.lengthPerByte);
-    while (counter < e) {
-      c = c * m;
-      counter.innerIncrement();
-    }
-    return c;
-  }
 
   //
   // Compute c = m^e mod n
@@ -34,18 +25,7 @@ class RSA {
     }
   }
 
-  //
-  static int computeA(int m, int e, int n) {
-    int c = m;
-    int counter = 1;
-
-    while (counter < e) {
-      c = c * m;
-      counter++;
-    }
-    c = c % n;
-    return c;
+  encrypt() {
+    
   }
-
-  encrypt() {}
 }
